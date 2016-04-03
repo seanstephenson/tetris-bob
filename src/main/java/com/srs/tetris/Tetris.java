898c9ed765437f0d955ea7cbfa001892ee67951d
Sean Stephenson <sstephenson@workfront.com>
2016-04-03 18:31:09 -0400

    Initial commit

---
+++ src/main/java/com/srs/tetris/Tetris.java
@@ -0,0 +1,11 @@
+package com.srs.tetris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tetris {
+	public static void main(String[] args) {
+		SpringApplication.run(Tetris.class, args);
+	}
+}
