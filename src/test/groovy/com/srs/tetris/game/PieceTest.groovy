9f60557cc58759ffaacded4e60475002b5e63e63
Sean Stephenson <sstephenson@workfront.com>
2016-05-22 17:44:19 -0400

    Add tests for the piece class

--- src/test/groovy/com/srs/tetris/game/PieceTest.groovy
+++ src/test/groovy/com/srs/tetris/game/PieceTest.groovy
@@ -5,21 +5,45 @@ import org.junit.Test
 class PieceTest {
 	@Test
 	void board() {
+		def piece = new Piece(PieceType.L, Color.Red)
+		assert piece.board.grid == PieceType.L.getBoard(0).grid
 
+		piece = piece.rotateLeft()
+		assert piece.board.grid == PieceType.L.getBoard(1).grid
 	}
 
 	@Test
 	void moveDown() {
-		// check that the piece moves, and the original is not changed
+		def piece = new Piece(PieceType.L, Color.Red, 0, 1, 2)
+		assert piece.x == 1
+		assert piece.y == 2
+
+		def moved = piece.moveDown()
+		assert moved.x == 1
+		assert moved.y == 3
+
+		// Check that the original does not change.
+		assert piece.x == 1
+		assert piece.y == 2
 	}
 
 	@Test
 	void rotateLeft() {
+		def piece = new Piece(PieceType.L, Color.Red, 0, 1, 2)
+		assert piece.orientation == 0
 
+		def rotated = piece.rotateLeft()
+		assert rotated.orientation == 1
+		assert piece.orientation == 0
 	}
 
 	@Test
 	void rotateRight() {
+		def piece = new Piece(PieceType.L, Color.Red, 0, 1, 2)
+		assert piece.orientation == 0
 
+		def rotated = piece.rotateRight()
+		assert rotated.orientation == -1
+		assert piece.orientation == 0
 	}
 }
