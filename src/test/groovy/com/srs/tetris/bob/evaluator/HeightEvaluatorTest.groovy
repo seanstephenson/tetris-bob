a45e8023cc9b890a26315ece61197f6ac138674f
Sean Stephenson <sstephenson@workfront.com>
2016-06-25 20:46:05 -0400

    Height evaluator test case

---
+++ src/test/groovy/com/srs/tetris/bob/evaluator/HeightEvaluatorTest.groovy
@@ -0,0 +1,32 @@
+package com.srs.tetris.bob.evaluator
+
+import com.srs.tetris.game.Board
+import org.junit.Test
+
+class HeightEvaluatorTest {
+	@Test
+	public void evaluate() {
+		HeightEvaluator evaluator = new HeightEvaluator()
+
+		assert evaluator.evaluate(new Board([
+		    [0, 0, 0],
+		    [0, 0, 0],
+		    [0, 0, 0],
+		    [0, 0, 0],
+		] as int[][])).score == 0.0
+
+		assert evaluator.evaluate(new Board([
+		    [0, 0, 0],
+		    [0, 0, 0],
+		    [0, 0, 0],
+		    [0, 0, 1],
+		] as int[][])).score == 1.0
+
+		assert evaluator.evaluate(new Board([
+		    [1, 0, 0],
+		    [0, 1, 0],
+		    [0, 1, 0],
+		    [1, 1, 1],
+		] as int[][])).score == 4.0
+	}
+}
