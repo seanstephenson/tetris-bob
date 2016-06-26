9aa584e11d05f529a32b285adc4eea30348b7e95
Sean Stephenson <sstephenson@workfront.com>
2016-06-25 20:50:26 -0400

    Completed lines evaluator test

---
+++ src/test/groovy/com/srs/tetris/bob/evaluator/CompletedLinesEvaluatorTest.groovy
@@ -0,0 +1,53 @@
+package com.srs.tetris.bob.evaluator
+
+import com.srs.tetris.game.Board
+import org.junit.Test
+
+class CompletedLinesEvaluatorTest {
+	@Test
+	public void evaluate() {
+		CompletedLinesEvaluator evaluator = new CompletedLinesEvaluator()
+
+		assert evaluator.evaluate(new Board([
+		    [0, 0, 0],
+		    [0, 0, 0],
+		    [0, 0, 0],
+		    [0, 0, 0],
+		] as int[][])).score == 0.0
+
+		assert evaluator.evaluate(new Board([
+		    [0, 0, 0],
+		    [0, 0, 0],
+		    [0, 0, 0],
+		    [1, 1, 0],
+		] as int[][])).score == 0.0
+
+		assert evaluator.evaluate(new Board([
+		    [0, 0, 0],
+		    [0, 0, 0],
+		    [0, 1, 0],
+		    [1, 1, 0],
+		] as int[][])).score == 0.0
+
+		assert evaluator.evaluate(new Board([
+		    [0, 0, 0],
+		    [0, 0, 0],
+		    [0, 1, 0],
+		    [1, 1, 1],
+		] as int[][])).score == 1.0
+
+		assert evaluator.evaluate(new Board([
+		    [0, 0, 0],
+			[1, 1, 1],
+		    [0, 1, 0],
+		    [1, 1, 1],
+		] as int[][])).score == 2.0
+
+		assert evaluator.evaluate(new Board([
+			[1, 1, 1],
+			[1, 1, 1],
+			[1, 1, 1],
+		    [1, 1, 1],
+		] as int[][])).score == 4.0
+	}
+}
