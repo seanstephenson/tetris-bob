ffe8ad37c0759081b16a438b093e4d6e46f8ef15
Sean Stephenson <sstephenson@workfront.com>
2016-07-04 00:34:12 -0400

    Updated weights for the danger zone

--- src/main/java/com/srs/tetris/bob/evaluator/SapientEvaluator.java
+++ src/main/java/com/srs/tetris/bob/evaluator/SapientEvaluator.java
@@ -5,10 +5,12 @@ package com.srs.tetris.bob.evaluator;
  */
 public class SapientEvaluator extends CompositeEvaluator {
 	private static final double HEIGHT_WEIGHT = -1.0;
-	private static final double DANGER_ZONE_WEIGHT = -5.0;
+	private static final double AVERAGE_HEIGHT_WEIGHT = -1.0;
+
+	private static final double DANGER_ZONE_WEIGHT = -4.0;
 	private static final double DANGER_ZONE_EXPONENT = 1.25;
 	private static final int DANGER_ZONE_SIZE = 10;
-	private static final double AVERAGE_HEIGHT_WEIGHT = -1.0;
+
 	private static final double HOLE_WEIGHT = -10.0;
 	private static final double HOLE_COVER_WEIGHT = -2.0;
 	private static final double NARROW_GAP_WEIGHT = -1.0;
@@ -16,8 +18,8 @@ public class SapientEvaluator extends CompositeEvaluator {
 	public SapientEvaluator() {
 		super(
 			new WeightedEvaluator(new HeightEvaluator(), HEIGHT_WEIGHT),
-			new WeightedEvaluator(new DangerZoneEvaluator(DANGER_ZONE_SIZE, DANGER_ZONE_EXPONENT), DANGER_ZONE_WEIGHT),
 			new WeightedEvaluator(new AverageHeightEvaluator(), AVERAGE_HEIGHT_WEIGHT),
+			new WeightedEvaluator(new DangerZoneEvaluator(DANGER_ZONE_SIZE, DANGER_ZONE_EXPONENT), DANGER_ZONE_WEIGHT),
 			new HolesEvaluator(HOLE_WEIGHT, HOLE_COVER_WEIGHT),
 			new WeightedEvaluator(new NarrowGapEvaluator(), NARROW_GAP_WEIGHT)
 		);
