8995d12c4389bc79d98e2cf2c780bcf8173c1783
Sean Stephenson <sstephenson@workfront.com>
2016-07-04 02:30:35 -0400

    Better weights

--- src/main/java/com/srs/tetris/bob/evaluator/SapientEvaluator.java
+++ src/main/java/com/srs/tetris/bob/evaluator/SapientEvaluator.java
@@ -4,16 +4,19 @@ package com.srs.tetris.bob.evaluator;
  * The best known combination of evaluators.
  */
 public class SapientEvaluator extends CompositeEvaluator {
-	private static final double HEIGHT_WEIGHT = -1.0;
-	private static final double AVERAGE_HEIGHT_WEIGHT = -1.0;
+
+	private static final double HEIGHT_WEIGHT = -1.5;
+	private static final double AVERAGE_HEIGHT_WEIGHT = -1.5;
 
 	private static final double DANGER_ZONE_WEIGHT = -4.0;
 	private static final double DANGER_ZONE_EXPONENT = 1.25;
 	private static final int DANGER_ZONE_SIZE = 10;
 
-	private static final double HOLE_WEIGHT = -10.0;
+	private static final double HOLE_WEIGHT = -15.0;
 	private static final double HOLE_COVER_WEIGHT = -2.0;
+
 	private static final double NARROW_GAP_WEIGHT = -1.0;
+
 
 	public SapientEvaluator() {
 		super(
