75913ed687cb8526f17f2da4d305e7f6357f5d80
Sean Stephenson <sstephenson@workfront.com>
2016-07-10 02:03:19 -0600

    Refactor board UI into a separate component

---
+++ src/main/java/com/srs/tetris/javafx/BoardPane.java
@@ -0,0 +1,140 @@
+package com.srs.tetris.javafx;
+
+import com.srs.tetris.game.GameBoard;
+import com.srs.tetris.game.Piece;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+
+import static com.srs.tetris.game.Color.*;
+
+/**
+ * A javafx pane that displays a game board.
+ */
+public class BoardPane extends Pane {
+	private static final double BOARD_BORDER = 1.0;
+	private static final Color BOARD_BORDER_COLOR = Color.web("0x202020");
+	private static final Color BOARD_BACKGROUND = Color.web("0x202020");
+
+	private static final double SQUARE_SIZE = 30.0;
+	private static final double SQUARE_GAP = 1.0;
+
+	private static final Color EMPTY_SQUARE = Color.web("0x303030");
+	private static final double GHOST_SQUARE_OPACITY = 0.3;
+
+	private int width, height;
+	private Rectangle[][] boardGrid;
+
+	public BoardPane(int width, int height) {
+		this.width = width;
+		this.height = height;
+		createComponents();
+	}
+
+	public BoardPane(GameBoard board) {
+		this(board.getWidth(), board.getHeight());
+		update(board);
+	}
+
+	private void createComponents() {
+		setBackground(new Background(new BackgroundFill(BOARD_BACKGROUND, null, null)));
+		setPrefSize(
+			width * (SQUARE_SIZE + SQUARE_GAP) + SQUARE_GAP + BOARD_BORDER * 2,
+			height * (SQUARE_SIZE + SQUARE_GAP) + SQUARE_GAP + BOARD_BORDER * 2
+		);
+		setBorder(new Border(new BorderStroke(BOARD_BORDER_COLOR, BorderStrokeStyle.SOLID, null, new BorderWidths(BOARD_BORDER))));
+
+		boardGrid = new Rectangle[width][height];
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				double left = x * (SQUARE_GAP + SQUARE_SIZE) + SQUARE_GAP + BOARD_BORDER;
+				double top = y * (SQUARE_GAP + SQUARE_SIZE) + SQUARE_GAP + BOARD_BORDER;
+
+				Rectangle square = new Rectangle(left, top, SQUARE_SIZE, SQUARE_SIZE);
+				getChildren().add(square);
+				boardGrid[x][y] = square;
+			}
+		}
+	}
+
+	/**
+	 * Updates this pane's display to match the state of the given game board.
+	 */
+	public void update(GameBoard board) {
+		if (width != board.getWidth() || height != board.getHeight()) throw new IllegalArgumentException();
+
+		// Set each square color.
+		for (int x = 0; x < board.getWidth(); x++) {
+			for (int y = 0; y < board.getHeight(); y++) {
+				setSquare(boardGrid[x][y], board.get(x, y));
+			}
+		}
+	}
+
+	/**
+	 * Draw the given piece on the board.
+	 */
+	public void drawPiece(Piece piece) {
+		drawPiece(piece, false);
+	}
+
+	/**
+	 * Draw the given piece on the board with a transparent color.
+	 */
+	public void drawTransparentPiece(Piece piece) {
+		drawPiece(piece, true);
+	}
+
+	public void drawPiece(Piece piece, boolean transparent) {
+		for (int pieceX = 0; pieceX < piece.getBoard().getWidth(); pieceX++) {
+			for (int pieceY = 0; pieceY < piece.getBoard().getHeight(); pieceY++) {
+				int boardX = pieceX + piece.getX(), boardY = pieceY + piece.getY();
+				if (!piece.getBoard().isEmpty(pieceX, pieceY)) {
+					setSquare(boardGrid[boardX][boardY], piece.getColor(), transparent);
+				}
+			}
+		}
+	}
+
+	private void setSquare(Rectangle square, com.srs.tetris.game.Color gameColor) {
+		setSquare(square, gameColor, false);
+	}
+
+	private void setSquare(Rectangle square, com.srs.tetris.game.Color gameColor, boolean transparent) {
+		if (gameColor != Empty) {
+			Color color = translateGameColor(gameColor).deriveColor(0, 1.0, 0.85, 1.0);
+			if (transparent) color = new Color(color.getRed(), color.getGreen(), color.getBlue(), GHOST_SQUARE_OPACITY);
+
+			// Do a linear gradient with lighter color at the upper left.
+			square.setFill(new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
+				new Stop(0.0, color.deriveColor(0, 1.0, 1.2, 1.0)), new Stop(0.4, color), new Stop(1.0, color)));
+
+		} else {
+			// Empty, just set it up here
+			square.setFill(EMPTY_SQUARE);
+		}
+	}
+
+	private Color translateGameColor(com.srs.tetris.game.Color gameColor) {
+		switch (gameColor) {
+			case Empty: return Color.TRANSPARENT;
+			case Red: return Color.RED;
+			case Green: return Color.GREEN;
+			case Blue: return Color.BLUE;
+			case Cyan: return Color.CYAN;
+			case Purple: return Color.PURPLE;
+			case Orange: return Color.ORANGE;
+			case Yellow: return Color.YELLOW;
+			default: throw new IllegalStateException();
+		}
+	}
+}
--- src/main/java/com/srs/tetris/javafx/TetrisLocal.java
+++ src/main/java/com/srs/tetris/javafx/TetrisLocal.java
@@ -1,4 +1,4 @@
-package com.srs.tetris;
+package com.srs.tetris.javafx;
 
 import com.srs.tetris.bob.BobPlayer;
 import com.srs.tetris.bob.learn.FileUtil;
@@ -9,7 +9,6 @@ import com.srs.tetris.game.GameSettings;
 import com.srs.tetris.game.Piece;
 import com.srs.tetris.player.DirectPlayer;
 import com.srs.tetris.player.LocalPlayer;
-import com.srs.tetris.player.Player;
 import com.srs.tetris.replay.ReplayUtil;
 import java.io.IOException;
 import java.text.NumberFormat;
@@ -28,15 +27,9 @@ import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import static com.srs.tetris.game.Color.*;
 
 /**
  * For testing, starts a tetris game with a local UI view.
@@ -45,20 +38,14 @@ public class TetrisLocal extends Application implements GameListener {
 
 	private static final Color ROOT_BACKGROUND = Color.web("0x262626");
 
-	private static final double BOARD_BORDER = 1.0;
-	private static final Color BOARD_BORDER_COLOR = Color.web("0x202020");
-	private static final Color BOARD_BACKGROUND = Color.web("0x202020");
-
-	private static final double SQUARE_SIZE = 30.0;
-	private static final double SQUARE_GAP = 1.0;
-
-	private static final Color EMPTY_SQUARE = Color.web("0x303030");
-	private static final double GHOST_SQUARE_OPACITY = 0.3;
+	private static final double INFO_BOX_BORDER = 1.0;
+	private static final Color INFO_BOX_BORDER_COLOR = Color.web("0x202020");
+	private static final Color INFO_BOX_BACKGROUND = Color.web("0x303030");
 
 	private Game game;
 
 	private Scene scene;
-	private Rectangle[][] boardGrid;
+	private BoardPane boardPane;
 
 	private Text scoreText;
 	private Text linesText;
@@ -106,29 +93,7 @@ public class TetrisLocal extends Application implements GameListener {
 
 	private Pane createUI() throws IOException {
 		// Create the board.
-		int width = game.getBoard().getWidth();
-		int height = game.getBoard().getHeight();
-
-		Pane boardPane = new Pane();
-		boardPane.setBackground(new Background(new BackgroundFill(BOARD_BACKGROUND, null, null)));
-		boardPane.setPrefSize(
-			width * (SQUARE_SIZE + SQUARE_GAP) + SQUARE_GAP + BOARD_BORDER * 2,
-			height * (SQUARE_SIZE + SQUARE_GAP) + SQUARE_GAP + BOARD_BORDER * 2
-		);
-		boardPane.setBorder(new Border(new BorderStroke(BOARD_BORDER_COLOR, BorderStrokeStyle.SOLID, null, new BorderWidths(BOARD_BORDER))));
-
-		boardGrid = new Rectangle[width][height];
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				double left = x * (SQUARE_GAP + SQUARE_SIZE) + SQUARE_GAP + BOARD_BORDER;
-				double top = y * (SQUARE_GAP + SQUARE_SIZE) + SQUARE_GAP + BOARD_BORDER;
-
-				Rectangle square = new Rectangle(left, top, SQUARE_SIZE, SQUARE_SIZE);
-				setSquare(square, Empty);
-				boardPane.getChildren().add(square);
-				boardGrid[x][y] = square;
-			}
-		}
+		boardPane = new BoardPane(game.getBoard());
 
 		// Create the info pane.
 		VBox infoPane = new VBox(15,
@@ -161,8 +126,8 @@ public class TetrisLocal extends Application implements GameListener {
 		infoBox.setAlignment(Pos.CENTER);
 		infoBox.setPrefWidth(150);
 		infoBox.setPadding(new Insets(5));
-		infoBox.setBackground(new Background(new BackgroundFill(EMPTY_SQUARE, null, null)));
-		infoBox.setBorder(new Border(new BorderStroke(BOARD_BORDER_COLOR, BorderStrokeStyle.SOLID, null, new BorderWidths(BOARD_BORDER))));
+		infoBox.setBackground(new Background(new BackgroundFill(INFO_BOX_BACKGROUND, null, null)));
+		infoBox.setBorder(new Border(new BorderStroke(INFO_BOX_BORDER_COLOR, BorderStrokeStyle.SOLID, null, new BorderWidths(INFO_BOX_BORDER))));
 
 		return infoBox;
 	}
@@ -199,67 +164,21 @@ public class TetrisLocal extends Application implements GameListener {
 			levelText.setText(formatter.format(game.getLevel()));
 			linesText.setText(formatter.format(game.getCompletedLines()));
 
-			// Draw the board.
-			GameBoard board = game.getBoard().clone();
+			// Update the game board.
+			GameBoard board = game.getBoard();
+			boardPane.update(board);
 
-			// Find where the piece would be if it dropped now.
+			// Find where the current piece would be if it dropped now.
 			Piece droppedPiece = game.getPiece();
 			while (board.canPlace(droppedPiece.moveDown())) {
 				droppedPiece = droppedPiece.moveDown();
 			}
 
-			// Now put the piece on the board where it is now.
-			board.place(game.getPiece());
-
-			// Set each square color.
-			for (int x = 0; x < board.getWidth(); x++) {
-				for (int y = 0; y < board.getHeight(); y++) {
-					setSquare(boardGrid[x][y], board.get(x, y));
-				}
-			}
-
 			// Draw a semi-transparent copy of the piece in the drop location.
-			for (int pieceX = 0; pieceX < droppedPiece.getBoard().getWidth(); pieceX++) {
-				for (int pieceY = 0; pieceY < droppedPiece.getBoard().getHeight(); pieceY++) {
-					int boardX = pieceX + droppedPiece.getX(), boardY = pieceY + droppedPiece.getY();
-					if (!droppedPiece.getBoard().isEmpty(pieceX, pieceY) && board.isEmpty(boardX, boardY)) {
-						setSquare(boardGrid[boardX][boardY], droppedPiece.getColor(), true);
-					}
-				}
-			}
+			boardPane.drawTransparentPiece(droppedPiece);
+
+			// Draw the current piece last (overwriting everything else).
+			boardPane.drawPiece(game.getPiece());
 		});
-	}
-
-	private void setSquare(Rectangle square, com.srs.tetris.game.Color gameColor) {
-		setSquare(square, gameColor, false);
-	}
-
-	private void setSquare(Rectangle square, com.srs.tetris.game.Color gameColor, boolean transparent) {
-		if (gameColor != Empty) {
-			Color color = translateGameColor(gameColor).deriveColor(0, 1.0, 0.85, 1.0);
-			if (transparent) color = new Color(color.getRed(), color.getGreen(), color.getBlue(), GHOST_SQUARE_OPACITY);
-
-			// Do a linear gradient with lighter color at the upper left.
-			square.setFill(new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
-				new Stop(0.0, color.deriveColor(0, 1.0, 1.2, 1.0)), new Stop(0.4, color), new Stop(1.0, color)));
-
-		} else {
-			// Empty, just set it up here
-			square.setFill(EMPTY_SQUARE);
-		}
-	}
-
-	private Color translateGameColor(com.srs.tetris.game.Color gameColor) {
-		switch (gameColor) {
-			case Empty: return Color.TRANSPARENT;
-			case Red: return Color.RED;
-			case Green: return Color.GREEN;
-			case Blue: return Color.BLUE;
-			case Cyan: return Color.CYAN;
-			case Purple: return Color.PURPLE;
-			case Orange: return Color.ORANGE;
-			case Yellow: return Color.YELLOW;
-			default: throw new IllegalStateException();
-		}
 	}
 }
